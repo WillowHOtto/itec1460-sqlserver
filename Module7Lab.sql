bc175f7202f018cec5024b00b22a4d51216ac11d
Willow Otto <pz8313ru@go.minnstate.edu>
2026-02-27 19:00:30 +0000

    Module7Lab PART2

--- Module7Lab.sql
+++ Module7Lab.sql
@@ -121,3 +121,117 @@ EXEC CalculateOrderTotal
 
 PRINT 'Returned total: $' + CAST(ISNULL(@TotalAmount, 0) AS NVARCHAR(20));
 
+
+
+USE Northwind;
+GO
+
+-- =============================================
+-- Part 2, Procedure 1: GetProductName
+-- =============================================
+
+CREATE OR ALTER PROCEDURE GetProductName
+    @ProductID INT,
+    @ProductName NVARCHAR(40) OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- YOUR CODE HERE: Write a SELECT statement that sets
+    -- @ProductName equal to the ProductName column
+    -- from the Products table where ProductID matches @ProductID.
+    SELECT @ProductName = ProductName
+    FROM Products
+    WHERE ProductID = @ProductID;
+
+    IF @ProductName IS NULL
+        PRINT 'Product not found.';
+    ELSE
+        PRINT 'Product Name: ' + @ProductName;
+END
+GO
+
+-- Test it
+DECLARE @ProductName NVARCHAR(40);
+
+EXEC GetProductName
+    @ProductID = 1,
+    @ProductName = @ProductName OUTPUT;
+GO
+
+
+-- =============================================
+-- Part 2, Procedure 2: GetEmployeeOrderCount
+-- =============================================
+
+CREATE OR ALTER PROCEDURE GetEmployeeOrderCount
+    @EmployeeID INT,
+    @OrderCount INT OUTPUT
+AS
+BEGIN
+
+     SET NOCOUNT ON;
+    
+    SELECT @OrderCount = COUNT(*)
+    FROM Orders
+    WHERE EmployeeID = @EmployeeID;
+    
+    PRINT 'Employee ' + CAST(@EmployeeID AS NVARCHAR(10)) + 
+          ' has processed ' + CAST(@OrderCount AS NVARCHAR(10)) + ' orders.';
+END
+GO
+
+
+-- Test it
+DECLARE @OrderCount INT;
+
+EXEC GetEmployeeOrderCount
+    @EmployeeID = 5,
+    @OrderCount = @OrderCount OUTPUT;
+GO
+
+
+-- =============================================
+-- Part 2, Procedure 3: CheckProductStock
+-- =============================================
+
+CREATE OR ALTER PROCEDURE CheckProductStock
+    @ProductID INT,
+    @NeedsReorder BIT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    -- Check if UnitsInStock is less than or equal to ReorderLevel
+    SELECT @NeedsReorder = CASE 
+        WHEN UnitsInStock <= ReorderLevel THEN 1 
+        ELSE 0 
+    END
+    FROM Products
+    WHERE ProductID = @ProductID;
+    
+    -- Print appropriate message based on the result
+    IF @NeedsReorder = 1
+        PRINT 'Product ' + CAST(@ProductID AS NVARCHAR(10)) + ' needs reordering.';
+    ELSE
+        PRINT 'Product ' + CAST(@ProductID AS NVARCHAR(10)) + ' stock is OK.';
+END
+GO
+
+-- Test CheckProductStock if a product NEEDS REORDER
+DECLARE @NeedsReorder BIT;
+
+EXEC CheckProductStock
+    @ProductID = 2,
+    @NeedsReorder = @NeedsReorder OUTPUT;
+
+GO
+
+-- Test CheckProductStock if a product DOES NOT NEED REORDER
+DECLARE @NeedsReorder BIT;
+
+EXEC CheckProductStock
+    @ProductID = 7,
+    @NeedsReorder = @NeedsReorder OUTPUT;
+
+GO
